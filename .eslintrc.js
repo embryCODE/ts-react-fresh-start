5dc3fa69567e79fc6fa79ac2ae1fdbea369dcb1e
Mason Embry <mason@embrycode.com>
2019-10-25 09:13:51 -0500

    Add and configure eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  // Note, each plugin overrides the plugins before it, as needed.
+  extends: [
+    'plugin:react/recommended', // Enforces good practices for React.
+    'prettier/@typescript-eslint', // Turns off eslint rules which conflict with Prettier.
+    'plugin:@typescript-eslint/recommended', // Enforces good practices for TypeScript, as recommended by the eslint people.
+
+    // Remove this if you don't want your formatting to show as an eslint error.
+    // I don't like that but you might.
+    'plugin:prettier/recommended', // Turns your formatting rules into actual eslint errors.
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  ecmaFeatures: {
+    jsx: true,
+  },
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
